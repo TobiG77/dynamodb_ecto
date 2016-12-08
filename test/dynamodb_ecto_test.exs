109a6fc7ad4f0c29a1cd13a85e1cc50106dbc223
Tobias Gerschner <tobias.gerschner@gmail.com>
2016-12-09 06:09:04 +1300

    remove default test case

--- test/dynamodb_ecto_test.exs
+++ test/dynamodb_ecto_test.exs
@@ -2,10 +2,6 @@ defmodule DynamodbEctoTest do
   use ExUnit.Case
   doctest DynamodbEcto
 
-  test "DynamoDB is up" do
-    assert 1 + 1 == 2
-  end
-
   test "should be able to create table" do
     ExAws.Dynamo.create_table("test_table", "id", %{id: :string}, 1, 1) |> ExAws.request
     {_resp, tables} = ExAws.Dynamo.list_tables |> ExAws.request
